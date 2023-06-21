df6bfc8510453d7adccb79fb6808dfcd69cd3375
lvhao09 <lvhao09@meituan.com>
2023-06-21 18:55:08 +0800

    first commit

---
+++ src/main/java/io/reflectoring/buckpal/application/port/in/GetAccountBalanceQuery.java
@@ -0,0 +1,9 @@
+package io.reflectoring.buckpal.application.port.in;
+
+import io.reflectoring.buckpal.application.domain.model.Account.AccountId;
+import io.reflectoring.buckpal.application.domain.model.Money;
+
+public interface GetAccountBalanceQuery {
+
+  Money getAccountBalance(AccountId accountId);
+}
